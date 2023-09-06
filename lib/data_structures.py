6bf143ffa57421cab079f75d815f03c5ea3c93e6
Kerry Guarino <kerry.guarino@gmail.com>
2023-09-05 20:31:02 -0400

    paused on print_spicy_foods

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -26,6 +26,7 @@ def get_spiciest_foods(spicy_foods):
     #returns list of dictionaries
 
 def print_spicy_foods(spicy_foods):
+    #new_list = here
     pass
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
